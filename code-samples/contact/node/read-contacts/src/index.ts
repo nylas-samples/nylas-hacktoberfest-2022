a4458a0ec3a31953079c906050778355c79c58d9
Daniele Vilela <danielevilela@outlook.com>
2022-10-03 18:59:23 -0700

    Update index.ts
    enforcing types

--- code-samples/contact/node/read-contacts/src/index.ts
+++ code-samples/contact/node/read-contacts/src/index.ts
@@ -1,14 +1,18 @@
 import  Nylas from "nylas";
+import 'dotenv/config';
+import Contact from "nylas/lib/models/contact";
 
 Nylas.config({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    clientId: process.env.CLIENT_ID as string,
+    clientSecret: process.env.CLIENT_SECRET as string,
 });
 
-const nylas = Nylas.with(process.env.ACCESS_TOKEN);
+const nylas = Nylas.with(process.env.ACCESS_TOKEN as string);
 
-nylas.contacts.list({limit: 10}).then(contacts => {
+nylas.contacts.list({limit: 10}).then((contacts: Contact[]) => {
   for (const contact of contacts) {
-      console.log(`Name: ${contact.givenName} ${contact.surname} | Email: ${contact.emailAddresses[0]['email']} | ID: ${contact.id}`);
+     const { id, givenName, surname, emailAddresses } = contact;
+     
+      console.log(`Name: ${givenName} ${surname} | Emails: ${emailAddresses?.join(',')} | ID: ${id}`);
   }
 });
