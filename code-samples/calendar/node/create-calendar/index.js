c2c0df488ea3f0b46319f551bf3a31d1cf878cbe
dkelxldk <aditiaaldama221@gmail.com>
2022-10-15 16:27:22 +0700

    fix(calendar): create

--- code-samples/calendar/node/create-calendar/index.js
+++ code-samples/calendar/node/create-calendar/index.js
@@ -1,30 +1,30 @@
-import Nylas from 'nylas';
-import dotenv from 'dotenv';
+const dotenv = require("dotenv");
+const Nylas = require("nylas");
+const { default: Calendar } = require("nylas/lib/models/calendar");
 
 dotenv.config();
 
 // Initiate config
 Nylas.config({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    accessToken: process.env.ACCESS_TOKEN
+    clientId: process.env.CLIENT_ID ,
+    clientSecret: process.env.CLIENT_SECRET 
 });
 
-// Create the calendar
-Nylas.calendars.create({
-    name: "Nylas Calendar",
-    description: "Nylas Calendar for #Hacktoberfest2022",
-    location: "Github",
-    timezone: "Asia/Jakarta"
-}).then((calendar) => {
-    // Succesfull result
-    console.log(
-        `Name: ${calendar.name}`,
-        `Description: ${calendar.description}`,
-        `Location: ${calendar.location}`,
-        `Timezone: ${calendar.timezone}`,
-    )
-}).catch((error) => {
-    // Error result
-    console.log(error.message)
+const nylas = Nylas.with(process.env.ACCESS_TOKEN );
+
+const calendar = new Calendar(nylas, {
+  name: 'Nylas #Hacktoberfest2022',
+  description: 'Nylas #Hacktoberfest2022',
+  location: 'Github',
+  timezone: 'Asia/Jakarta'
 });
+
+calendar.save()
+    .then((c) => {
+        console.log(`Calendar created successfully`)
+        console.log(`Name : ${c.name}`);
+        console.log(`Description : ${c.description}`);
+        console.log(`Location : ${c.location}`);
+        console.log(`Timezone : ${c.timezone}`);
+    })
+    .catch((err) => console.log(err));
